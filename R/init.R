a873fd6c134859e1521e4c86023a70307c7d9fb2
giacfalk <giacomo.falchetta@gmail.com>
2022-03-25 16:13:25 +0100

    Bugfix

--- R/init.R
+++ R/init.R
@@ -5,6 +5,33 @@ dl_func <- function(url, output, username, password) {
 }
 
 #' @export
+odr_login_bm <- function (credentials, source, verbose = TRUE)
+{
+  if (!inherits(credentials, "character") || length(credentials) !=
+      2) {
+    stop("credentials must be a vector character string of length 2 (username and password)\n")
+  }
+  .testInternetConnection()
+  if (verbose) {
+    cat("Checking credentials...\n")
+  }
+  if (source == "earthdata") {
+    x <- httr::GET(url = "https://opendap.cr.usgs.gov/opendap/hyrax/MOD11A2.006/h17v07.ncml.ascii?time",
+                   httr::authenticate(user = credentials[1], credentials[2]),
+                   config = list(maxredirs = -1), timeout(30))
+    httr::stop_for_status(x, "login to Earthdata. Check out username and password")
+    httr::warn_for_status(x)
+    options(earthdata_user = credentials[1])
+    options(earthdata_pass = credentials[2])
+    options(earthdata_odr_login = TRUE)
+  }
+  if (verbose) {
+    cat("Successfull login to", source, "\n")
+  }
+}
+
+
+#' @export
 bm_initialize <- function(username, password){
 
 require(sf)

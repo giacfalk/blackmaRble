02a010214df691a24c8bde246f2a52c264212e6f
Giacomo Falchetta <36954873+giacfalk@users.noreply.github.com>
2023-12-21 17:42:30 +0100

    Update bm_get_data.R

--- R/bm_get_data.R
+++ R/bm_get_data.R
@@ -40,13 +40,13 @@ df <- data.frame(date_range, year, month, day, yday)
 for (i in 1:nrow(df)){
   tryCatch({
   print(paste0("Downloading file ", i, " of ", nrow(df)*length(tile_index)))
-  csv <- read_csv(paste0("https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/5000/", data_product, "/", df$year[i], "/", ifelse(nchar(df$yday[i])==1, paste0("00", df$yday[i]), ifelse(nchar(df$yday[i])==2, paste0("0", df$yday[i]), df$yday[i])), ".csv"), col_types = cols())
+  csv <- read_csv(paste0("https://ladsweb.modaps.eosdis.nasa.gov/opendap/RemoteResources/laads/allData/5000/", data_product, "/", df$year[i], "/", ifelse(nchar(df$yday[i])==1, paste0("00", df$yday[i]), ifelse(nchar(df$yday[i])==2, paste0("0", df$yday[i]), df$yday[i])), ".csv"), col_types = cols())
 
   file <- as.character(csv$name[grep(paste(tile_index,collapse="|"), csv$name)])
 
   for (j in 1:length(file)){
 
-    link <- as.character(paste0("https://ladsweb.modaps.eosdis.nasa.gov/opendap/hyrax/allData/5000/", data_product, "/", as.character(df$year[i]), "/", as.character(ifelse(nchar(df$yday[i])==1, paste0("00", df$yday[i]), ifelse(nchar(df$yday[i])==2, paste0("0", df$yday[i]), df$yday[i]))),  "/", file[j], ".nc4"))
+    link <- as.character(paste0("https://ladsweb.modaps.eosdis.nasa.gov/opendap/RemoteResources/laads/allData/5000/", data_product, "/", as.character(df$year[i]), "/", as.character(ifelse(nchar(df$yday[i])==1, paste0("00", df$yday[i]), ifelse(nchar(df$yday[i])==2, paste0("0", df$yday[i]), df$yday[i]))),  "/", file[j], ".nc4"))
 
     dl_func(link, paste0(file.path(Sys.getenv("USERPROFILE"),"Desktop",fsep="\\"), "\\", paste0(file[j], ".nc4")), username, password)
   }  }, error=function(e){})
